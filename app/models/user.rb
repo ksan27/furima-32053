c172f1ddd5302a30e52869e64ba98c08cbb3b8dd
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-18 16:58:04 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}+\z/i, message: "Password Include both letters and numbers"}
+    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "First name Full-width characters"}
+    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Last name Full-width characters"}
+    validates :first_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters"}
+    validates :last_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters"}
+  end
+
 end
