14d5f914d7169ac6e8e491c661ebda1c2183d5e0
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-19 19:39:12 +0900

    ActiveHashのモデル記述

---
+++ app/models/item.rb
@@ -0,0 +1,19 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :shipping_prefecture
+  belongs_to :shipping_day
+
+  belongs_to :user
+
+  validates :name, :explanation, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :shipping_prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1}
+
+end
