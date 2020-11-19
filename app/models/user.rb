25816a42e6481342b0d4c9998c715d201ca86005
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-19 14:26:06 +0900

    validatesの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-
   with_options presence: true do
+    validates :nickname, presence: true
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'Password Include both letters and numbers' }
     validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'First name Full-width characters' }
     validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Last name Full-width characters' }
     validates :first_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'First name kana Full-width katakana characters' }
     validates :last_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Last name kana Full-width katakana characters' }
+    validates :birthday, presence: true
   end
 end
