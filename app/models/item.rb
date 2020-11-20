8a1afd27d59ac1d8f44720962aff0cb675b54d79
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-20 12:04:58 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,9 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
   belongs_to :user
+  has_one_attached :image
 
-  validates :name, :explanation, :price, presence: true
+  validates :name, :explanation, :price, :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
--- app/models/shipping_prefecture.rb
+++ app/models/shipping_prefecture.rb
@@ -2,7 +2,7 @@ class ShippingPrefecture < ActiveHash::Base
   self.data = [
     { id: 1, name: '--'},
     { id: 2, name: '北海道'},
-    { id: 3, name: '青森県'}
+    { id: 3, name: '青森県'},
     { id: 4, name: '岩手県'},
     { id: 5, name: '宮城県'},
     { id: 6, name: '秋田県'},
