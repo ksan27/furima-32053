362a643fd9798910ae9739f59362d00569c79af5
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-20 14:18:02 +0900

    ビューの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :explanation, :price, :image, presence: true
-
+with_options presence: true do
+  validates :image, :name, :explanation
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
   validates :shipping_fee_id, numericality: { other_than: 1 }
   validates :shipping_prefecture_id, numericality: { other_than: 1 }
   validates :shipping_day_id, numericality: { other_than: 1}
+  validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
 end
