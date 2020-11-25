ba2c55b341694c2e58bd5be27891ad219429eb62
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-25 21:23:51 +0900

    商品に関するビュー編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,17 +1,23 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new(purchase_params)
     if @item_purchase.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
       @item_purchase.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render action: :index
     end
@@ -20,10 +26,19 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params
-    .require(:item_purchase)
-    .permit(:item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
-    .merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:item_purchase)
+          .permit(:item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user == @item.user || @item.purchase.present?
+      redirect_to root_path
+    end
   end
 
 end
