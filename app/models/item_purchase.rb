28f32d2a89ab1d2a264b54fb7bfb1bc67589b352
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-25 21:24:46 +0900

    単体テストコードの実装

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -7,14 +7,16 @@ class ItemPurchase
                 :building_name, 
                 :phone_number, 
                 :user_id, 
-                :item_id
+                :item_id,
+                :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :city, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "City Full-width characters"}
+    validates :token
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select"}
+    validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: "is invalid. Input half-width characters." }
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: "Input only number" }
   end
 
   def save
